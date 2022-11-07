5113d90516b44cb079f49034584a0f8c443bdd3b
Yy115 <72635442+Yy115@users.noreply.github.com>
2022-11-07 23:21:39 +0800

    Update AxiosClient.js

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://54.234.147.205:8080',
+  baseURL: 'http://54.234.147.205:8999',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
