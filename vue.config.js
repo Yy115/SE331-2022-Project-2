0e3ab975384dd56e73a52d53244377b00ba1b9e5
Chen Yang <yc2261124363@outlook.com>
2022-11-07 22:56:10 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  lintOnSave: false,
   transpileDependencies: true,
   devServer: {
-    port: 3000
+    port: 4000
   }
 })
